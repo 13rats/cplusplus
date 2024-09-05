06342decba62d3754b85131327b6a77ac071cce8
U-BSD\419746 <419746@SHS-JYBF6X3.beaverton.k12.or.us>
2024-09-05 09:30:43 -0700

    yo ho ho

--- guessinggame.cpp
+++ guessinggame.cpp
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-using namespace std;
+using namespace std; // Guessing Game code by Zach Russell, 9/5/2024 - Selects a random number and helps you guess it with under/over
 
 int main(){
   int input = 0;
@@ -11,14 +11,16 @@ int main(){
   srand(time(NULL));
   compNum = rand() % 100 + 1;
   cout << "guess what number i'm thinking of. 1-100" << endl;
-  cout << compNum << endl;
   while (input != compNum){
     cin >> input;
     if (input == compNum){
       cout << "you got it! I was thinking of " << compNum << endl;
     }
-    else {
-      cout << "not quite, try again!" << endl;
+    else if (input > compNum){
+      cout << "number is too high, try again!" << endl;
+    }
+    else if (input < compNum){
+      cout << "number is too low, try again!" << endl;
     }
   }
   cout << "would you like tp play again (y/n)" << endl;
